bbabfbcff64cd3661dee72097730c1c2140735ba
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-13 16:40:15 +0900

    asd

---
+++ binaryxScript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name    BinaryX-Test
+// @version 1
+// @include https://market.binaryx.pro/*
+// @include http://market.binaryx.pro/*
+// @include https://market-m.binaryx.pro/*
+// @include http://market-m.binaryx.pro/*
+// @include https://www.binaryx.pro/*
+// @include http://www.binaryx.pro/*
+// @supportURL   https://github.com/Boritgoge/BinaryX
+// @updateURL    https://github.com/Boritgoge/BinaryX/master/binaryxScript.js
+// @downloadURL  https://github.com/Boritgoge/BinaryX/master/binaryxScript.js
+// @require https://vuejs.org/js/vue.min.js
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// @require https://raw.githubusercontent.com/Boritgoge/BinaryX/master/binaryx.js
+// @grant   GM_xmlhttpRequest
+// @connect pro-api.coinmarketcap.com
+// @connect raw.githubusercontent.com
+// ==/UserScript==
+BinaryX.init();
