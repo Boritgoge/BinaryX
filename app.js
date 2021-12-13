318ccaff1dc028a44734d2461845f4d82209a532
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-13 15:25:32 +0900

    asd

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const jq = $.noConflict(true);
-
-const vm = new Vue({
-    el: 'test2',
-    data: function(){
-        return {
-            test2:'test2'
+jq(document).ready(function(){
+    const vm = new Vue({
+        el: 'test2',
+        data: function(){
+            return {
+                test2:'test2'
+            }
+        },
+        mounted(){
+            this.test2();
+        },
+        methods: {
+            test2() {
+                alert("test22222");
+            }
         }
-    },
-    mounted(){
-        this.test2();
-    },
-    methods: {
-        test2() {
-            alert("test22222");
-        }
-    }
-});
+    });
+})
